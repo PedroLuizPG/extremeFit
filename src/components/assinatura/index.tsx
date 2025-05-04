99d391ee92287b2efd6f3bf7f830970018c5a8a0
Pedro <peuluiz.13@gmail.com>
2025-05-03 23:22:51 -0300

    pequeno ajuste

--- src/components/assinatura/index.tsx
+++ src/components/assinatura/index.tsx
@@ -3,7 +3,7 @@ import { BsInstagram } from "react-icons/bs"
 export function Assinatura(){
     return(
         <div className="w-full mx-auto text-center bg-[#121212] text-white ">
-            <h4 className="italic p-2 flex items-center justify-center gap-2">Desenvolded by <a href="https://www.insth4gram.com/peuluiz_/" className="flex items-center justify-center hover:underline duration-300 gap-2 hover:animate-bounce max-w-fit"><BsInstagram/> @peuluiz_ </a> </h4>
+            <h4 className="italic p-2 flex items-center justify-center gap-2">Desenvolded by <a href="https://www.instagram.com/peuluiz_/" className="flex items-center justify-center hover:underline duration-300 gap-2 hover:animate-bounce max-w-fit"><BsInstagram/> @peuluiz_ </a> </h4>
             
         </div>
     )
